882fbedc2a5c2c6ec2542f46bb09a3806aa1e491
Ines <link_ines@gmx.de>
2024-01-10 13:55:49 +0100

    created html,css

--- main.js
+++ main.js
@@ -3,7 +3,21 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
+window.addEventListener('DOMContentLoaded', async() =>{
+  const   RESPONSE = await fetch('./data.json')
+  async function fetchDataObject (){
+
+  }
+})
+const searchField = document.getElementById("search")
+/* const searchInput = event.target.value */
+
+
+
+
+
+
+/* document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
@@ -21,4 +35,4 @@ document.querySelector('#app').innerHTML = `
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector('#counter')) */
