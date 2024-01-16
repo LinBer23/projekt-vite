71dcdc1915f2b1909fc16b007cd60116ea57960d
Ines <link_ines@gmx.de>
2024-01-16 13:59:00 +0100

    change title and toLowerCase  for the search

--- main.js
+++ main.js
@@ -1,38 +1,32 @@
 window.addEventListener("DOMContentLoaded", async () => {
-        const RESPONSE = await fetch("https://restcountries.com/v3.1/all");
-        const responseJson = await RESPONSE.json();// json()(von json) parst in array
-        const gridWrapper = document.querySelector("grid-body");
-        console.log(responseJson[0]);
-            for (const land of responseJson) {
-                const item = document.createElement("section");
+    const RESPONSE = await fetch("https://restcountries.com/v3.1/all");
+    const responseJson = await RESPONSE.json(); // json()(von json) parst in array
+    const gridWrapper = document.querySelector("grid-body");
+    console.log(responseJson[0]);
+    for (const land of responseJson) {
+        const item = document.createElement("section");
 
-                item.innerHTML += `<figure><img src=${land.flags.png} alt="flag of ">
-                <h2>${land.name.common}</h2>
-                <p><b>Population: </b>${land.population.toLocaleString()}</p>
-                <p><b>Region: </b> ${land.region} 
-                <p><b>Capital: </b>${land.capital}</figure>`;
-                gridWrapper.appendChild(item);
-                }
-                
+        item.innerHTML += `<figure><img src=${land.flags.png} alt="flag of ">
+            <h2>${land.name.common}</h2>
+            <p><b>Population: </b>${land.population.toLocaleString()}</p>
+            <p><b>Region: </b> ${land.region} 
+            <p><b>Capital: </b> ${land.capital}</figure>`;
+        gridWrapper.appendChild(item);
+    }
+    const inputField = document.getElementById("countries");
+    const selectField = document.getElementById("continentSelection");
 
+    inputField.addEventListener("input", (event) => {
+        const inputValue = event.target.value;
+        responseJson.filter((countries) => {
+            if (countries.name.common.toLowerCase().includes(inputValue.toLowerCase())) {
+                return console.log(countries.name.common, inputValue);
+            }
+        });
+
+        selectField.addEventListener("change", (event) => {
+            const valueSelect = event.target.value;
+            console.log(valueSelect);
+        });
+    });
 });
-/* capital name */
-const inputField = document.getElementById("countries")
-const selectField =document.getElementById("continentSelection")
-
-inputField.addEventListener("input",(event) =>{
-   const inputValue = event.target.value;
-})
-
-selectField.addEventListener("change",(event) =>{
-    const valueSelect = event.target.value
-    console.log(valueSelect)
-})
-
-
-
-
-
-
-
-
